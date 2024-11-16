b532a818a9bb11f6b619716b6600579f919abb9b
Axel Quintana <axelquintana369@gmail.com>
2024-11-16 12:25:18 -0300

    Puntos obligatorios terminados

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -3,20 +3,25 @@
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
+from ..transport import transport # Importamos transport.py para acceder a la funcion getAllImages()
+
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
+    json_collection = transport.getAllImages(input) # obtenemos los datos de la API
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
+
+    for object in json_collection: # recorremos los datos obtenidos y los sumamos a la lista images en formato de card usando a translator
+        images.append(translator.fromRequestIntoCard(object))
 
     return images
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
-    fav = '' # transformamos un request del template en una Card.
-    fav.user = '' # le asignamos el usuario correspondiente.
+    fav = translator.fromTemplateIntoCard(request) # transformamos un request del template en una Card.
+    fav.user = get_user(request) # le asignamos el usuario correspondiente.
 
     return repositories.saveFavourite(fav) # lo guardamos en la base.
 
@@ -27,11 +32,11 @@ def getAllFavourites(request):
     else:
         user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
+        favourite_list = repositories.getAllFavourites(user) # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
+            card = translator.fromRepositoryIntoCard(favourite) # transformamos cada favorito en una Card, y lo almacenamos en card.
             mapped_favourites.append(card)
 
         return mapped_favourites
